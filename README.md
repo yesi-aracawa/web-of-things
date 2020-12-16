1fe4b70b586d17eaf8b056a8ee6b8e0e127fc490
yesi-aracawa <yesi_yamanaka@hotmail.com>
2020-12-16 03:41:23 -0600

    feat: front-B4 NPM scripts part 1
    change json scripts to call css and sass

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # web-of-things
 WOT has many common elements on web sites, schedule, menu, selectors, etc.
+
+## JSON SCRIPTS
+
+### Run multiple NPM scripts in parallel using parallelshell NPM module.
+#### Windows
+  * "watch:scss": "onchange \"css/*.scss\" -- npm run scss",
+  * "watch:all": "parallelshell \"npm run watch:scss\" \"npm run lite\"",
+
+#### MacOs or Linux
+  * "watch:scss": "onchange 'css/*.scss' -- npm run scss",
+  * "watch:all": "parallelshell 'npm run watch:scss' 'npm run lite'"
---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$("#carouselButton").click(function () {
+  if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+    $("#mycarousel").carousel("pause");
+    $("#carouselButton").children("span").removeClass("fa-pause");
+    $("#carouselButton").children("span").addClass("fa-play");
+  } else if ($("#carouselButton").children("span").hasClass("fa-play")) {
+    $("#mycarousel").carousel("cycle");
+    $("#carouselButton").children("span").removeClass("fa-play");
+    $("#carouselButton").children("span").addClass("fa-pause");
+  }
+});
+$("#loginBtn").click(function () {
+  $("#loginModal").modal("show");
+});
+$("#reserveBtn").click(function () {
+  $("#reserveTableModal").modal("toggle");
+});
